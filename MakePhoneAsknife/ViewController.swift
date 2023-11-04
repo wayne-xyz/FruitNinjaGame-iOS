9e67fc4d86671b5fbd1123287567095e4a564aae
rongwei <rongwei.ji@outlook.com>
2023-11-04 12:12:28 -0500

    update the moving function and icon

--- MakePhoneAsknife/ViewController.swift
+++ MakePhoneAsknife/ViewController.swift
@@ -24,6 +24,7 @@
 import UIKit
 import MultipeerConnectivity
 import CoreLocation
+import CoreMotion
 
 
 class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManagerDelegate{
@@ -32,9 +33,9 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
     @IBOutlet weak var conncetLable: UILabel!
     
 
-        var LabelopenMacApp: String="Please open the Mac app to connect"
-        var Labelconnecting: String="Connecting"
-        var Labelconnected:String="Connected you can press start"
+    var LabelopenMacApp: String="Please open the Mac app to connect"
+    var Labelconnecting: String="Connecting"
+    var Labelconnected:String="Connected you can press start"
         
  
     
@@ -44,20 +45,30 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
     
     func didChangeConnectionState(peer: MCPeerID, isConnected: Bool) {
         //update the label to prompt the connect situation
-        conncetLable.text=Labelconnecting
+        DispatchQueue.main.async {
+            // Perform UI updates on the main thread
+            self.conncetLable.text=self.Labelconnecting
+        }
+       
         if isConnected{
             if let targets = ConnectManager.shared.session?.connectedPeers, !targets.isEmpty{
-                conncetLable.text=Labelconnected
+                DispatchQueue.main.async {
+                    // Perform UI updates on the main thread
+                    self.conncetLable.text=self.Labelconnected
+                }
+                
             }
         }else{
-            conncetLable.text=LabelopenMacApp
+            DispatchQueue.main.async {
+                // Perform UI updates on the main thread
+                self.conncetLable.text=self.LabelopenMacApp
+            }
         }
     }
     
     // CLLocationManager properti
     var locationManager: CLLocationManager!
-    
-    
+
     @IBAction func showPopup(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Game Guide", message: "Just open the Mac app to connect, and press the start game to move your phone as the Shuriken, Go Ninja!", preferredStyle: .alert)
         
@@ -75,7 +86,7 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
     @IBAction func startGameButtonTapped(_ sender: UIButton) {
         if let targets = ConnectManager.shared.session?.connectedPeers, !targets.isEmpty{
             
-            let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(sendMassage), userInfo: nil, repeats: true)
+            
             
         }else{
             conncetLable.text=LabelopenMacApp
@@ -86,12 +97,12 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
     // property for last location
     var previousLocation: CLLocation?
     
-    
+    let motionManager = CMMotionManager()
+    // for init the conncet and motion
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // init CLLocationManager
-        
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -101,17 +112,63 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
         ConnectManager.shared.delegate = self
         ConnectManager.shared.start()
         
-        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(sendMassage), userInfo: nil, repeats: true)
+       
+        // for test
+        //_ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(sendMassage), userInfo: nil, repeats: true)
         
+        startAccelerometerUpdates()
     }
     
-    @objc func sendMassage(){
-//        
-//        if   let targets=ConnectManager.shared.session?.connectedPeers{
-//            ConnectManager.shared.send(message: "hi i am ios app", to: targets)
-//        }else{
-//            
-//        }
+    func startAccelerometerUpdates() {
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 1.0 / 60.0  // Update interval (e.g., 60 times per second)
+            motionManager.startAccelerometerUpdates(to: OperationQueue.main) { (accelerometerData, error) in
+                if let acceleration = accelerometerData?.acceleration {
+                    // Process the accelerometer data here
+                    
+                    let xAcceleration = acceleration.x
+                    let yAcceleration = acceleration.y
+                    let zAcceleration = acceleration.z
+                    
+                    // Calculate the magnitude of acceleration
+                    let accelerationMagnitude = sqrt(xAcceleration * xAcceleration + yAcceleration * yAcceleration + zAcceleration * zAcceleration)
+                    
+                    // Detect significant changes in acceleration
+                    let accelerationThreshold: Double = 1.1
+                    if accelerationMagnitude > accelerationThreshold {
+                        // Respond to the detected movement
+                        print("Device is moving!")
+                        print("x:\(xAcceleration),y:\(yAcceleration),z:\(zAcceleration)")
+                        var message="\(xAcceleration),\(yAcceleration),\(zAcceleration)"
+                        self.sendMassage(message)
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func stopAccelerometerUpdates() {
+          motionManager.stopAccelerometerUpdates()
+    }
+    
+    
+    
+    func sendMassage(_ message:String){
+        print("Start sending ")
+        if   let targets=ConnectManager.shared.session?.connectedPeers{
+            ConnectManager.shared.send(message: message, to: targets)
+        }else{
+            print("Send failed")
+        }
+        
+        
+          
+    }
+    
+    
+    // it's not gps
+    func originalSendMessage(){
         if let targets = ConnectManager.shared.session?.connectedPeers,
             let location = locationManager.location {
             //optional check targests and location
@@ -125,28 +182,29 @@ class ViewController: UIViewController, ConnectManagerDelegate,CLLocationManager
                 previousLocation = location // update the previous location
 
                 // save the location dictionary
-                let locationInfo: [String: Any] = [
+            let locationInfo: [String: Any] = [
                     "xCoordinate": xCoordinate,
                     "yCoordinate": yCoordinate,
                     "direction": direction,
                     "speed": speed
-                ]
-
+            ]
+            print(locationInfo)
             do {
                 // change the location to string
                 let jsonData = try JSONSerialization.data(withJSONObject: locationInfo, options: [])
                 if let jsonString = String(data: jsonData, encoding: .utf8) {
                     // send string
                     ConnectManager.shared.send(message: jsonString, to: targets)
+                    print("send succefully\(jsonString)")
                 }
             } catch {
                 print("Error encoding locationInfo: \(error)")
             }
                 
-            }
-          
+        }else{
+            conncetLable.text=LabelopenMacApp
+        }
     }
-    
     
     func calculateDirection(from: CLLocation, to: CLLocation) -> String {
         // get start and end loction
